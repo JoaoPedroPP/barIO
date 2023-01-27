1fa6fa280501511736180713ef5c62844917e583
Joao Pedro Poloni Ponce <joaopedropp@MacBook-Pro-de-Joao-2.local>
2023-01-27 10:28:26 -0300

    :sparkles:

--- src/manipulation/mod.rs
+++ src/manipulation/mod.rs
@@ -1,7 +1,11 @@
 mod data;
 mod process;
 mod cos;
+mod redis;
 
 pub use data::Hist;
+pub use data::URedisPayload;
+pub use data::URedisResponse;
 pub use process::convert;
 pub use cos::get_image;
+pub use redis::save_redis;
