5236df7612921ed02aeef3597fd14a71b872c5fc
JoaoPedroPP <jp052@hotmail.com>
2023-01-28 14:27:52 -0300

    :zap:

--- src/main.rs
+++ src/main.rs
@@ -31,7 +31,6 @@ fn main() {
 
     loop {
         let img: String = manipulation::convert("pikachu_p.jpg");
-        // manipulation::convert("test.png");
         match manipulation::save_redis(img) {
             Ok(_) => println!("Sucesso no redis"),
             Err(_) => println!("Sem sucesso no redis"),
