ba4e2128f297d55d95a56e948767ea69ef16c96b
JoaoPedroPP <jp052@hotmail.com>
2023-03-03 15:13:09 -0300

    :heavy_plus_sign:

--- src/manipulation/redis.rs
+++ src/manipulation/redis.rs
@@ -2,10 +2,10 @@ use std::env;
 // use serde_json;
 use super::URedisPayload;
 
-pub fn save_redis(value: String) ->Result<(), u16> {
+pub fn save_redis(key: String, value: String) ->Result<(), u16> {
     let uredis_url = env::var("uREDIS_URL").expect("uREDIS_URL not set");
     let url = format!("{uredis}/api/cache", uredis=uredis_url);
-    let payload: URedisPayload = URedisPayload::new("dasdasdads", value);
+    let payload: URedisPayload = URedisPayload::new(&key, value);
     let client = reqwest::blocking::Client::new();
     let resp = client.post(url)
         .json(&payload)
